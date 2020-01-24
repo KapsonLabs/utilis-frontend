7675d814c4ae0eff3e7b2e3032077c6a7fd0c2a5
Allan Katongole <kapsonkatongole@gmail.com>
2020-01-24 12:37:39 +0300

    Fix widget with token

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Now UI Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
@@ -24,7 +7,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/now-ui-dashboard.scss?v1.2.0";
 import "assets/css/demo.css";
 
-import AdminLayout from "layouts/Admin.jsx";
 import Landing from "layouts/Landing.jsx";
 
 const hist = createBrowserHistory();
@@ -32,7 +14,6 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/landing" component={Landing} />
       <Redirect to="/landing" />
     </Switch>
--- src/layouts/Landing.jsx
+++ src/layouts/Landing.jsx
@@ -1,21 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Web3Connect from "web3connect";
-import { ethers } from 'ethers';
-import Web3 from 'web3';
-import Portis from "@portis/web3";
-
-import {
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    FormGroup,
-    Form,
-    Input,
-    Row,
-    Col
-  } from "reactstrap";
 
 class Landing extends React.Component{
 
@@ -29,9 +12,11 @@ class Landing extends React.Component{
     launchWidget = () => {
         window.AirSwapInstant.render(
             { // DAI token address
-              token: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
-              amount: '50.429',
-              mode: 'sell',
+              env: 'development',
+              token: '0xf68abfbdd621b5699d930da00cb673e6a913a8fe',
+              baseToken: 'DAI',
+            //   amount: '50',
+              mode: 'buy',
               onComplete: txId => console.log('great success!', txId),
               onClose: () => console.log('cancelled prod trade'),
             },
@@ -48,33 +33,7 @@ class Landing extends React.Component{
                 </div>
 
                 <div className="container" >
-                    <Web3Connect.Button
-                        label = {"Connect Wallet"}
-                        providerOptions={{
-                            portis: {
-                                package: Portis, // required
-                                options: {
-                                id: "7b4b6647-0d2d-4ae2-8597-a34fbcdaa4fa" // required
-                                }
-                            },
-                            //   fortmatic: {
-                            //     package: Fortmatic, // required
-                            //     options: {
-                            //       key: "FORTMATIC_KEY" // required
-                            //     }
-                            //   },
-                        }}
-
-                        onConnect={(provider) => {
-                            const web3 = new Web3(provider); // add provider to web3
-                            // const web3 = new ethers.providers.Web3Provider(provider);
-                            this.launchWidget()
-                        }}
-
-                        onClose={() => {
-                            console.log("Web3Connect Modal Closed"); // modal has closed
-                        }}
-                    />
+                   <button class="btn btn-success" onClick={this.launchWidget}>Launch Trader</button>
                 </div>
             </div>
         )
