70b7a41c749bf097f83a3446e7af851083d11ecc
Allan Katongole <kapsonkatongole@gmail.com>
2020-02-12 13:33:07 +0300

    Final design

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Row, Col} from "react-bootstrap";
+import { Grid, Row, Col, Navbar, Nav, NavDropdown} from "react-bootstrap";
 import Navigation from "./Navigation";
 
 class Home extends React.Component{
@@ -8,22 +8,60 @@ class Home extends React.Component{
         super(props);
         this.state = {
           connected: false,
-          token: ''
+          token: 'DAI'
         }
     }
 
+    setWeth = () => {
+        this.setState({token: 'WETH'})
+    }
+
+    setDai = () => {
+        this.setState({token: 'DAI'})
+    }
+
+    setUsdc = () => {
+        this.setState({token: 'USDC'})
+    }
+
+
     launchWidget = () => {
+        if(this.state.token==="DAI"){
         window.AirSwapInstant.render(
             { 
               env: 'development',
               token: '0xf68abfbdd621b5699d930da00cb673e6a913a8fe',
               mode: 'buy',
-              baseToken: 'WETH',
+              baseToken: 'DAI',
               onComplete: txId => console.log('great success!', txId),
               onClose: () => console.log('cancelled prod trade'),
             },
             'body',
-         ) 
+         )}
+        if(this.state.token==="WETH"){
+            window.AirSwapInstant.render(
+                { 
+                  env: 'development',
+                  token: '0xf68abfbdd621b5699d930da00cb673e6a913a8fe',
+                  mode: 'buy',
+                  baseToken: 'WETH',
+                  onComplete: txId => console.log('great success!', txId),
+                  onClose: () => console.log('cancelled prod trade'),
+                },
+                'body',
+             )}
+        if(this.state.token==="USDC"){
+            window.AirSwapInstant.render(
+                { 
+                  env: 'development',
+                  token: '0xf68abfbdd621b5699d930da00cb673e6a913a8fe',
+                  mode: 'buy',
+                  baseToken: 'USDC',
+                  onComplete: txId => console.log('great success!', txId),
+                  onClose: () => console.log('cancelled prod trade'),
+                },
+                'body',
+             )}
     }
 
     render() {
@@ -31,7 +69,24 @@ class Home extends React.Component{
             <div className="content">
                 <Row>
                     <Col md="12" lg="12" sm="12">
-                        <Navigation/>
+                        <Navbar bg="light" expand="lg">
+                            <Navbar.Brand href="#home">Test Dapp</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto nav-design">
+                                <Nav.Link href="#home" className="link-design">Home</Nav.Link>
+                                <Nav.Link href="#link" className="link-design">dApp</Nav.Link>
+                                <Nav.Link href="#link" className="link-design">How To</Nav.Link>
+                                <NavDropdown title="Select Token" id="basic-nav-dropdown" className="link-design">
+                                    <NavDropdown.Item onClick={this.setWeth}>WETH</NavDropdown.Item>
+                                    <NavDropdown.Item onClick={this.setDai}>DAI</NavDropdown.Item>
+                                <NavDropdown.Item onClick={this.setUsdc}>USDC</NavDropdown.Item>
+                                {/* <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+                                </NavDropdown>
+                            </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
                     </Col>
                     <Col md="12">
                         <div className="bg">
